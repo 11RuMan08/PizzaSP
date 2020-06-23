3fe8f2d466d5c32cbcec3a69df87384ffa700570
Andrey Rumiantsev <rumiantsev.andrej2014@gmail.com>
2020-06-23 14:32:50 +0300

    add new functions

--- src/main/java/com/epam/pizza/controller/OrderController.java
+++ src/main/java/com/epam/pizza/controller/OrderController.java
@@ -1,9 +1,14 @@
 package com.epam.pizza.controller;
 
+import com.epam.pizza.model.order.Order;
+import com.epam.pizza.model.order.PayMethod;
+import com.epam.pizza.model.products.Drink;
+import com.epam.pizza.model.products.pizza;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -20,6 +25,45 @@ public class OrderController {
     public List getProducts() {
         return null;
     }
+    @GetMapping("/GetUserInfo")
+    public List getUserInfo(Order order) {
+        List<String> info = new ArrayList<>();
+        info.add(order.getName());
+        info.add(order.getAddress());
+        info.add(order.getPhoneNumber());
+        return info;
+    }
+
+    @GetMapping("/GetUserPayMethod")
+    public PayMethod getPayMethod(Order order) {
+       return order.getPayMethod();
+    }
+
+    @GetMapping("/GetDrinkInfo")
+    public List getDrinkInfo(Drink drink) {
+        List<String> info = new ArrayList<>();
+        info.add(drink.getName());
+        info.add(Integer.toString(drink.getSize()));
+        info.add(Integer.toString(drink.getValue()));
+        return info;
+    }
+
+    @GetMapping("/GetPizzaInfo")
+    public List getPizzaInfo(pizza prod) {
+        List<String> info = new ArrayList<>();
+        info.add(prod.getName());
+        info.add(Integer.toString(prod.getValue()));
+        info.add(Integer.toString(prod.getCcal()));
+        info.add(prod.getPizzaType());
+        info.add(prod.getNutritionalValue());
+        return info;
+    }
+
+    @GetMapping("/GetPizzaIngredients")
+    public List getPizzaIngredients(pizza prod) {
+        return prod.getIngredients();
+    }
+
 
     @PostMapping("/CreateOrder")
     public boolean createOrder() {
