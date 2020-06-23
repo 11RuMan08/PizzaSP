3b984a937bc7cd72137d533f9b23805ec0922b6d
Andrey Rumiantsev <rumiantsev.andrej2014@gmail.com>
2020-06-23 23:21:03 +0300

    add get function

--- src/main/java/com/epam/pizza/controller/OrderController.java
+++ src/main/java/com/epam/pizza/controller/OrderController.java
@@ -1,61 +1,37 @@
 package com.epam.pizza.controller;
 
 import com.epam.pizza.model.products.Drink;
+import com.epam.pizza.model.products.Ingredients;
 import com.epam.pizza.model.products.pizza;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class OrderController {
 
     private static final String HELLO_WORLD = "Test";
+    private List<Ingredients> ingredient = new ArrayList<Ingredients>(Arrays.asList(Ingredients.CHEESE,Ingredients.TOMATO,Ingredients.CHICKEN));
+    private Drink drink_obj = new Drink("Morse",500,1);
+    private pizza pizza_obj = new pizza("4 cheeses",450,"open","14/18/16",283,ingredient);
 
     @GetMapping("/helloWorld")
     public String helloWorld() {
         return HELLO_WORLD;
     }
-/*
-    @GetMapping("/GetProducts")
-    public List getProducts() {
-        return null;
-    }
-    @GetMapping("/GetUserInfo")
-    public List getUserInfo(Order order) {
-        List<String> info = new ArrayList<>();
-        info.add(order.getName());
-        info.add(order.getAddress());
-        info.add(order.getPhoneNumber());
-        return info;
-    }
-
-    @GetMapping("/GetUserPayMethod")
-    public PayMethod getPayMethod(Order order) {
-       return order.getPayMethod();
-    }
- */
 
     @GetMapping("/GetDrinkInfo")
-    public List getDrinkInfo(Drink drink) {
-        List<String> info = new ArrayList<>();
-        info.add(drink.getName());
-        info.add(Integer.toString(drink.getSize()));
-        info.add(Integer.toString(drink.getValue()));
-        return info;
+    public Drink getDrinkInfo() {
+        return drink_obj;
     }
 
     @GetMapping("/GetPizzaInfo")
-    public List getPizzaInfo(pizza prod) {
-        List<String> info = new ArrayList<>();
-        info.add(prod.getName());
-        info.add(Integer.toString(prod.getValue()));
-        info.add(Integer.toString(prod.getCcal()));
-        info.add(prod.getPizzaType());
-        info.add(prod.getNutritionalValue());
-        return info;
+    public pizza getPizzaInfo() {
+        return pizza_obj;
     }
 /*
     @GetMapping("/GetPizzaIngredients")
